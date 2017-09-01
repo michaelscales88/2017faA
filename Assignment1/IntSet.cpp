bf816c53ebc20277bb0cde77407d4027205eb72a
Michael Scales <mscales@mindwireless.com>
2017-09-01 12:02:35 -0500

    testing vim spacing

--- Assignment1/IntSet.cpp
+++ Assignment1/IntSet.cpp
@@ -60,15 +60,15 @@ bool IntSet::isEmpty() const
 
 bool IntSet::contains(int anInt) const
 {
-	bool found = false;
-	if (used > 0)
+bool found = false;
+if (used > 0)
+{
+	for (int i = 0; i < used; i++)
 	{
-		for (int i = 0; i < used; i++)
-	{	
-		found = (data[i] == anInt);
-			// Stop looking if found
-			if (found) { break; }
-		}
+	found = (data[i] == anInt);
+	// Stop looking if found
+	if (found) { break; }
+	}
 	}
 	return found;
 }

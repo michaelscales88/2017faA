d4884e82e54c18a6d211dd7ca1ba71c3771bc92d
Michael Scales <mscales@mindwireless.com>
2017-09-20 13:39:39 -0500

    removed documentation from top of Assignment2/IntSet.cpp

--- Assignment2/IntSet.cpp
+++ Assignment2/IntSet.cpp
@@ -1,76 +1,8 @@
-// FILE: IntSet.cpp - header file for IntSet class
-//       Implementation file for the IntStore class
-//       (See IntSet.h for documentation.)
-// INVARIANT for the IntSet class:
-// (1) Distinct int values of the IntSet are stored in a 1-D,
-//     dynamic array whose size is stored in member variable
-//     capacity; the member variable data references the array.
-// (2) The distinct int value with earliest membership is stored
-//     in data[0], the distinct int value with the 2nd-earliest
-//     membership is stored in data[1], and so on.
-//     Note: No "prior membership" information is tracked; i.e.,
-//           if an int value that was previously a member (but its
-//           earlier membership ended due to removal) becomes a
-//           member again, the timing of its membership (relative
-//           to other existing members) is the same as if that int
-//           value was never a member before.
-//     Note: Re-introduction of an int value that is already an
-//           existing member (such as through the add operation)
-//           has no effect on the "membership timing" of that int
-//           value.
-// (4) The # of distinct int values the IntSet currently contains
-//     is stored in the member variable used.
-// (5) Except when the IntSet is empty (used == 0), ALL elements
-//     of data from data[0] until data[used - 1] contain relevant
-//     distinct int values; i.e., all relevant distinct int values
-//     appear together (no "holes" among them) starting from the
-//     beginning of the data array.
-// (6) We DON'T care what is stored in any of the array elements
-//     from data[used] through data[capacity - 1].
-//     Note: This applies also when the IntSet is empry (used == 0)
-//           in which case we DON'T care what is stored in any of
-//           the data array elements.
-//     Note: A distinct int value in the IntSet can be any of the
-//           values an int can represent (from the most negative
-//           through 0 to the most positive), so there is no
-//           particular int value that can be used to indicate an
-//           irrelevant value. But there's no need for such an
-//           "indicator value" since all relevant distinct int
-//           values appear together starting from the beginning of
-//           the data array and used (if properly initialized and
-//           maintained) should tell which elements of the data
-//           array are actually relevant.
-//
-// DOCUMENTATION for private member (helper) function:
-//   void resize(int new_capacity)
-//     Pre:  (none)
-//           Note: Recall that one of the things a constructor
-//                 has to do is to make sure that the object
-//                 created BEGINS to be consistent with the
-//                 class invariant. Thus, resize() should not
-//                 be used within constructors unless it is at
-//                 a point where the class invariant has already
-//                 been made to hold true.
-//     Post: The capacity (size of the dynamic array) of the
-//           invoking IntSet is changed to new_capacity...
-//           ...EXCEPT when new_capacity would not allow the
-//           invoking IntSet to preserve current contents (i.e.,
-//           value for new_capacity is invalid or too low for the
-//           IntSet to represent the existing collection),...
-//           ...IN WHICH CASE the capacity of the invoking IntSet
-//           is set to "the minimum that is needed" (which is the
-//           same as "exactly what is needed") to preserve current
-//           contents...
-//           ...BUT if "exactly what is needed" is 0 (i.e. existing
-//           collection is empty) then the capacity should be
-//           further adjusted to 1 or DEFAULT_CAPACITY (since we
-//           don't want to request dynamic arrays of size 0).
-//           The collection represented by the invoking IntSet
-//           remains unchanged.
-//           If reallocation of dynamic array is unsuccessful, an
-//           error message to the effect is displayed and the
-//           program unconditionally terminated.
-
+/* Programmer: Michael Scales
+ * Assignment 2
+ * Data Structures
+ * 9/20/2017
+ */
 #include "IntSet.h"
 #include <iostream>
 #include <cassert>
